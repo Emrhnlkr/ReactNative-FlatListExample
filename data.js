0084fa8a5fa7fbc0050c772fb7d32b35a1bed874
Emrhnlkr <39797013+Emrhnlkr@users.noreply.github.com>
2019-12-02 18:07:26 +0300

    FlatList MYWORK
    React native ile flatlist ve search uygulaması

---
+++ data.js
@@ -0,0 +1,218 @@
+export default [
+	{
+		"_id": "5c52de4c0f4ed2ae1bff8739",
+		"picture": "https://i.pravatar.cc/301",
+		"name": "Webster Burgess",
+		"company": "EQUITOX"
+	},
+	{
+		"_id": "5c52de4cac35a17d8c0993c1",
+		"picture": "https://i.pravatar.cc/300",
+		"name": "Medina Glenn",
+		"company": "CYCLONICA"
+	},
+	{
+		"_id": "5c52de4c54f2cb9b2eaca28e",
+		"picture": "https://i.pravatar.cc/666",
+		"name": "Ernestine Reyes",
+		"company": "YURTURE"
+	},
+	{
+		"_id": "5c52de4c27a7eaf7b64d44ca",
+		"picture": "https://i.pravatar.cc/664",
+		"name": "Patrica Hess",
+		"company": "MENBRAIN"
+	},
+	{
+		"_id": "5c52de4c470775a1e4041379",
+		"picture": "https://i.pravatar.cc/667",
+		"name": "Lawrence Sherman",
+		"company": "SCENTRIC"
+	},
+	{
+		"_id": "5c52de4c491139efcc4d75cf",
+		"picture": "https://i.pravatar.cc/630",
+		"name": "Tessa Erickson",
+		"company": "AUSTECH"
+	},
+	{
+		"_id": "5c52de4cb83143009752997c",
+		"picture": "https://i.pravatar.cc/200",
+		"name": "Crystal Doyle",
+		"company": "ZILCH"
+	},
+	{
+		"_id": "5c52de4c4e4ae4ea190a4743",
+		"picture": "https://i.pravatar.cc/201",
+		"name": "Reyna Bradley",
+		"company": "CUIZINE"
+	},
+	{
+		"_id": "5c52de4c916c0ec176cc25ab",
+		"picture": "https://i.pravatar.cc/202",
+		"name": "Estelle Gallagher",
+		"company": "INTERODEO"
+	},
+	{
+		"_id": "5c52de4cf9da515d14735190",
+		"picture": "https://i.pravatar.cc/203",
+		"name": "Hendrix Rojas",
+		"company": "MAKINGWAY"
+	},
+	{
+		"_id": "5c52de4c42ef39a57f1bd1ee",
+		"picture": "https://i.pravatar.cc/204",
+		"name": "Kimberly Nieves",
+		"company": "XERONK"
+	},
+	{
+		"_id": "5c52de4cfdf070b1c0f5ed34",
+		"picture": "https://i.pravatar.cc/205",
+		"name": "Felecia Mcleod",
+		"company": "LIMAGE"
+	},
+	{
+		"_id": "5c52de4c157415a10ad2f39e",
+		"picture": "https://i.pravatar.cc/206",
+		"name": "Enid Blankenship",
+		"company": "ECLIPTO"
+	},
+	{
+		"_id": "5c52de4c94f0146ee1da0a39",
+		"picture": "https://i.pravatar.cc/207",
+		"name": "Rhodes Blanchard",
+		"company": "HAIRPORT"
+	},
+	{
+		"_id": "5c52de4ccae10cfc264ea5e0",
+		"picture": "https://i.pravatar.cc/208",
+		"name": "Craig Woodard",
+		"company": "KAGE"
+	},
+	{
+		"_id": "5c52de4c47d12f28075e233b",
+		"picture": "https://i.pravatar.cc/209",
+		"name": "Mathews Clements",
+		"company": "HALAP"
+	},
+	{
+		"_id": "5c52de4c277356ce5898add2",
+		"picture": "https://i.pravatar.cc/210",
+		"name": "Patti Burke",
+		"company": "MAGNEATO"
+	},
+	{
+		"_id": "5c52de4c3da0e66a28848c49",
+		"picture": "https://i.pravatar.cc/211",
+		"name": "Catalina Thompson",
+		"company": "AFFLUEX"
+	},
+	{
+		"_id": "5c52de4cdc5398d1d0e2e613",
+		"picture": "https://i.pravatar.cc/212",
+		"name": "Soto Dale",
+		"company": "POSHOME"
+	},
+	{
+		"_id": "5c52de4c281e927761f597d8",
+		"picture": "https://i.pravatar.cc/213",
+		"name": "Downs Williams",
+		"company": "HANDSHAKE"
+	},
+	{
+		"_id": "5c52de4ca2eba72ff8b19bbf",
+		"picture": "https://i.pravatar.cc/214",
+		"name": "Townsend Mckee",
+		"company": "UNIWORLD"
+	},
+	{
+		"_id": "5c52de4c8abcc5cfdf8a62f7",
+		"picture": "https://i.pravatar.cc/215",
+		"name": "Washington Serrano",
+		"company": "ZUVY"
+	},
+	{
+		"_id": "5c52de4cd45a1e52209e3d35",
+		"picture": "https://i.pravatar.cc/216",
+		"name": "Georgia Dixon",
+		"company": "PEARLESSA"
+	},
+	{
+		"_id": "5c52de4ca30e999fd297f483",
+		"picture": "https://i.pravatar.cc/217",
+		"name": "Mona Parker",
+		"company": "INSURITY"
+	},
+	{
+		"_id": "5c52de4c135e3122a92916e4",
+		"picture": "https://i.pravatar.cc/218",
+		"name": "Slater Brewer",
+		"company": "VORTEXACO"
+	},
+	{
+		"_id": "5c52de4c09d3e98855c67981",
+		"picture": "https://i.pravatar.cc/219",
+		"name": "Reilly Barron",
+		"company": "ANDERSHUN"
+	},
+	{
+		"_id": "5c52de4c43f2985efaca8ca8",
+		"picture": "https://i.pravatar.cc/220",
+		"name": "Doreen Cummings",
+		"company": "CODAX"
+	},
+	{
+		"_id": "5c52de4c188e45c9d2bcb429",
+		"picture": "https://i.pravatar.cc/221",
+		"name": "Hood Flores",
+		"company": "ORGANICA"
+	},
+	{
+		"_id": "5c52de4c5ede5c3037c54a39",
+		"picture": "https://i.pravatar.cc/222",
+		"name": "Pansy Tucker",
+		"company": "SHOPABOUT"
+	},
+	{
+		"_id": "5c52de4c0a312078fdf62dc9",
+		"picture": "https://i.pravatar.cc/223",
+		"name": "Joanne Phillips",
+		"company": "CIRCUM"
+	},
+	{
+		"_id": "5c52de4c28d9507099a79f85",
+		"picture": "https://i.pravatar.cc/224",
+		"name": "Duncan Roberson",
+		"company": "EPLOSION"
+	},
+	{
+		"_id": "5c52de4c5717df3361de5d9e",
+		"picture": "https://i.pravatar.cc/225",
+		"name": "Kemp Ward",
+		"company": "XINWARE"
+	},
+	{
+		"_id": "5c52de4cbf590fd683fde1f3",
+		"picture": "https://i.pravatar.cc/226",
+		"name": "Scott Peterson",
+		"company": "GEOFORMA"
+	},
+	{
+		"_id": "5c52de4c853220c9458f9f34",
+		"picture": "https://i.pravatar.cc/227",
+		"name": "Adriana Carter",
+		"company": "COMTRACT"
+	},
+	{
+		"_id": "5c52de4c2b503a2e56f755aa",
+		"picture": "https://i.pravatar.cc/228",
+		"name": "Fannie Travis",
+		"company": "TELEPARK"
+	},
+	{
+		"_id": "5c52de4c8fca3d8a5f821c1d",
+		"picture": "https://i.pravatar.cc/229",
+		"name": "Josefa Atkinson",
+		"company": "VIAGRAND"
+	},
+]
